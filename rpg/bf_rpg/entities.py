d45e9ca6d92d93503c736e283764d13ca9029bbb
Michal Leszczynski <mleszczynsk@gmail.com>
2019-10-02 23:05:18 +0200

    Added entities

---
+++ rpg/bf_rpg/entities.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass, field
+from typing import List, Dict
+
+
+class BaseEntity:
+    pass
+
+
+@dataclass(frozen=True)
+class Param(BaseEntity):
+    id: int
+    name: str
+    max_value: int
+    current_value: int
+
+
+@dataclass(frozen=True)
+class Skill(BaseEntity):
+    id: int
+    name: str
+    description: str
+    self_effect: Dict[Param, int] = field(default_factory=dict)
+    enemy_effect: Dict[Param, int] = field(default_factory=dict)
+
+
+@dataclass(frozen=True)
+class Trait(BaseEntity):
+    id: int
+    name: str
+    affected_params: List[Param] = field(default_factory=list)
+    gained_skills: List[Skill] = field(default_factory=list)
+
+
+@dataclass(frozen=True)
+class Item(BaseEntity):
+    id: int
+    name: str
+    value: int
+    weight: int
+    traits: List[Trait] = field(default_factory=list)
+
+
+@dataclass(frozen=True)
+class Character(BaseEntity):
+    id: int
+    name: str
+    items: List[Item] = field(default_factory=list)
+    traits: List[Trait] = field(default_factory=list)
+    skills: List[Skill] = field(default_factory=list)
+    params: List[Param] = field(default_factory=list)
+
+
+
+
+
+
