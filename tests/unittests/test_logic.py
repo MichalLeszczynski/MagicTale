b023cbf9ba37f95536f5976dcd57a11353de396f
Michal Leszczynski <mleszczynsk@gmail.com>
2019-10-09 22:05:21 +0200

    added first unit tests

--- tests/unittests/test_logic.py
+++ tests/unittests/test_logic.py
@@ -1,28 +1,50 @@
-from tests.unittests import StaticRepositories
+from rpg.bf_rpg.logic import CharacterLogic, ItemLogic, ParamLogic, SkillLogic, TraitLogic
+
+from ram_db.bf_ram_db.character import CharacterRepositories
+from ram_db.bf_ram_db.item import ItemRepositories
+from ram_db.bf_ram_db.param import ParamRepositories
+from ram_db.bf_ram_db.skill import SkillRepositories
+from ram_db.bf_ram_db.trait import TraitRepositories
 
 
-def test_add_item_increase_character_hp(
-    prepare_repositories: StaticRepositories
-) -> None:
-    orders, products, clients = prepare_repositories
-    order = orders.get(1)
-    logic = OrderLogic(orders=orders, products=products, clients=clients)
+def test_create_character() -> None:
 
-    assert order.total_cost == 0
+    logic = CharacterLogic(CharacterRepositories())
+    character = logic.create_new_character("Test Character")
 
-    logic.add_product(order_id=1, product_id=1)
+    assert logic.characters.get(character.id) == character
+    assert character.name == "Test Character"
 
-    order = orders.get(1)
-    assert order.total_cost == 100
 
-#
-# def test_add_product_increase_orders_total_cost_minimal(
-#     prepare_repositories: StaticRepositories
-# ) -> None:
-#     from bf_shop.logic import OrderLogic
-#
-#     logic = OrderLogic(*prepare_repositories)
-#
-#     order = logic.add_product(order_id=1, product_id=1)
-#
-#     assert order.total_cost == 100
+def test_create_item() -> None:
+
+    logic = ItemLogic(ItemRepositories())
+    item = logic.create_new_item(name="Test Item")
+
+    assert logic.items.get(item.id) == item
+    assert item.name == "Test Item"
+
+
+def test_create_param() -> None:
+
+    logic = ParamLogic(ParamRepositories())
+    param = logic.create_new_param(name="Test Param")
+
+    assert logic.params.get(param.id) == param
+    assert param.name == "Test Param"
+
+
+def test_create_skill() -> None:
+    logic = SkillLogic(SkillRepositories())
+    skill = logic.create_new_skill(name="Test Skill")
+
+    assert logic.skills.get(skill.id) == skill
+    assert skill.name == "Test Skill"
+
+
+def test_create_trait() -> None:
+    logic = TraitLogic(TraitRepositories())
+    trait = logic.create_new_trait(name="Test Trait")
+
+    assert logic.traits.get(trait.id) == trait
+    assert trait.name == "Test Trait"
